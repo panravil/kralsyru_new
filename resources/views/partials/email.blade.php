3ac7e3a8cceb1128d1e100ce73c7f384c6d2b1ae
panravil <panravil.dev@gmail.com>
2021-06-03 09:51:21 +0200

    Revert "Migrate MVC"
    This reverts commit 15eed2a824eaf3261c113b36f02167c894a4ca71.

--- resources/views/partials/email.blade.php
+++
@@ -1,4 +0,0 @@
-
-Email: {{$data['email']}}<br>
-Name: {{$data['name']}}<br>
-Message: {{$data['message']}}<br>
--- routes/web.php
+++ routes/web.php
@@ -13,72 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-Route::group(['middleware' => 'web'], function () {
-	Route::get('/', 'HomeController@index');
-	Route::get('/parovani-syry/{url}', 'CheeseController@index');
-	Route::get('/parovani-vina/{url}', 'WineController@index');
-	Route::get('/nase-syry', 'OurcheeseController@index');
-
-	Route::get('/nase-syry/{url}', 'ViewcheeseController@index');
-
-	Route::get('/syry-a-vino', 'CheesewineController@index');
-
-	Route::get('/hermelin', function () {
-		return view('pages.hermelin');
-	});
-	Route::get('/pece-o-syry', 'CheesecareController@index');
-
-	Route::get('/pribeh-krale', function () {
-		return view('pages.storyking');
-	});
-	Route::get('/nas-pribeh', function () {
-		return view('pages.story95');
-	});
-	Route::get('/95-vyroci', function () {
-		return view('pages.story95');
-	});
-
-	// Route::get('/jak-chutna-laska', function() {
-	//     return view('pages.love-taste');
-	// });
-
-	Route::get('/dozral-cas-na-lasku', function () {
-		return view('pages.love-time');
-	});
-
-	Route::get('/leto-kralovskych-zazitku', function () {
-		return view('pages.summer-experiences');
-	});
-
-	Route::get('/kontakt', 'ContactController@index');
-	Route::post('/kontakt', 'ContactController@sendForm');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-
-// Route::group(['prefix' => 'admin'], function () {
-//     Voyager::routes();
-// });
 
 Route::group(['prefix' => 'admin'], function () {
-	Voyager::routes();
-	Route::get('/hlavni-fotografie', 'CustomVoyagerController@mainPhotos')->middleware('admin.user');
-	Route::POST('/save-hlavni-fotografie', 'CustomVoyagerController@saveMainPhotos')->middleware('admin.user');
-	Route::get('/delete-hlavni-fotografie', 'CustomVoyagerController@deleteMainPhotos')->middleware('admin.user');
-	Route::POST('/change-hlavni-fotografie', 'CustomVoyagerController@changeMainPhotos')->middleware('admin.user');
-
-	Route::get('/profil-fotografie', 'CustomVoyagerController@profilePhotos')->middleware('admin.user');
-	Route::POST('/save-profil-fotografie', 'CustomVoyagerController@saveProfilePhotos')->middleware('admin.user');
-	Route::get('/delete-profil-fotografie', 'CustomVoyagerController@deleteProfilePhotos')->middleware('admin.user');
-	Route::POST('/change-profil-fotografie', 'CustomVoyagerController@changeProfilePhotos')->middleware('admin.user');
-
-	Route::get('/baleni-fotografie', 'CustomVoyagerController@baleniPhotos')->middleware('admin.user');
-	Route::POST('/save-baleni-fotografie', 'CustomVoyagerController@saveBaleniPhotos')->middleware('admin.user');
-	Route::get('/delete-baleni-fotografie', 'CustomVoyagerController@deleteBaleniPhotos')->middleware('admin.user');
-	Route::POST('/change-baleni-fotografie', 'CustomVoyagerController@changeBaleniPhotos')->middleware('admin.user');
-
-	Route::get('/nase-syry', 'CustomVoyagerController@ourCheese')->middleware('admin.user');
-	Route::POST('/save-nase-syry', 'CustomVoyagerController@saveOurCheese')->middleware('admin.user');
+    Voyager::routes();
 });
-
-
